771d7c6818e2137479f96ba1085bd94d614277b0
sese22 <tienloinguyen22@gmail.com>
2019-07-20 20:34:40 +0700

    Css

--- js-introduction/intro.js
+++ js-introduction/intro.js
@@ -1,1 +1,17 @@
 console.log('Hello world !!!');
+
+// get button element
+const registerButton = document.querySelector('.register-button');
+console.log(registerButton);
+
+// add event listener
+registerButton.addEventListener('click', () => {
+  const usernameElement = document.querySelector('.username-input');
+  const username = usernameElement.value;
+
+  if (!username) {
+    // show error message
+    const errorMessageElement = document.querySelector(`.error-message`);
+    errorMessageElement.innerText = 'Please input username'
+  }
+});
